4b07601450346316d424757d1fd43e572aa8d960
ali ahmed <aliahmedmohamed@gmail.com>
2022-07-04 23:17:42 +0200

    Set Crud Interface

---
+++ graduaion_project_backed/Repo/ICrud.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace graduaion_project_backed.Repo
+{
+    public interface ICrud<T>
+    {
+        int Add(T New);
+        int Delete(int id);
+        int Edit(int id, T New);
+        List<T> GetAll();
+        T GetById(int id);
+    }
+}
