a0727972b8e870f354ce72fae9e939ebdea221d7
ali ahmed <aliahmedmohamed@gmail.com>
2022-07-11 16:32:13 +0200

    branchfront

--- graduaion_project_backed/front/src/App.js
+++ graduaion_project_backed/front/src/App.js
@@ -6,6 +6,7 @@ import {
 import ShowCities from './components/cities/ShowCities';
 import AddCity from './components/cities/addCity';
 import AddStatus from './components/Status/AddStatus';
+import ShowBranches from './components/Branches/ShowBranches';
 
 function App() {
   console.log(process.env.base)
@@ -15,6 +16,7 @@ function App() {
         <Route path="/cities" element= {<ShowCities/>}></Route>
         <Route path="/addCity" element= {<AddCity/>}></Route>
         <Route path="/AddStatus" element= {<AddStatus/>}></Route>
+        <Route path="/branches" element= {<ShowBranches/>}></Route>
       </Routes>
     </div>
   );
