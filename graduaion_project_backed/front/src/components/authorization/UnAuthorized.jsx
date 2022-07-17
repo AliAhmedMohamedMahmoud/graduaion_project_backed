3361a0c2054bd11acedce7fe9520f7cf3945f590
ali ahmed <aliahmedmohamed@gmail.com>
2022-07-18 00:24:17 +0200

    unauthrize

--- graduaion_project_backed/front/src/components/authorization/UnAuthorized.jsx
+++ graduaion_project_backed/front/src/components/authorization/UnAuthorized.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function UnAuthorized() {
   return (
-    <div className=' text-center'>UnAuthorized</div>
+    <div>
+        <img className="w-100 h-25" src="https://hoganhost.com.ng/blog/wp-content/uploads/2021/02/403.png"></img>
+    </div>
   )
 }
