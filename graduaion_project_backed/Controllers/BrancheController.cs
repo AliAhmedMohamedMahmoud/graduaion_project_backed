b8d01e7312b0e6502ed310b32d7b6155835730c2
moham <moham@DESKTOP-GGTLRRB>
2022-07-17 21:52:08 +0200

    dummy-commit

--- graduaion_project_backed/Controllers/BrancheController.cs
+++ graduaion_project_backed/Controllers/BrancheController.cs
@@ -42,7 +42,7 @@ namespace graduaion_project_backed.Controllers
         }
 
         [HttpPost]
-    //    [RequestFilter("Add", "Branshes")]
+        [RequestFilter("Add", "Branch")]
         public IActionResult addBranch(Branches branche)
         {
             try
@@ -58,7 +58,7 @@ namespace graduaion_project_backed.Controllers
         }
 
         [HttpPut("{id}")]
-        [RequestFilter("Update", "Branshes")]
+        [RequestFilter("Update", "Branch")]
         public IActionResult PutBranchById(int id, Branches branche)
         {
             if (ModelState.IsValid == true)
--- graduaion_project_backed/front/src/components/LoginAndRegister/Register.jsx
+++ graduaion_project_backed/front/src/components/LoginAndRegister/Register.jsx
@@ -2,76 +2,71 @@
 import { Fragment, useEffect, useState } from 'react';
 import { getAll } from '../../Services/Roles';
 import { useNavigate } from "react-router-dom";
-import {register} from '../../Services/LogIn&Register'
+import { register } from '../../Services/LogIn&Register'
 import validator from 'validator';
 import "./Register.css";
 export default function Register() {
   const navigate = useNavigate();
   const [Roles, setRoles] = useState([]);
-  const[userName, SetUserName] = useState("")
-  const[password, Setpassword] = useState("")
-  const[ConfirmPassword, SetConfirmPassword] = useState("")
-  const[RoleName, SetRoleName] = useState("")
-  
-  const[IsValiduserName, SetIsValidUserName] = useState(true)
-  const[IsValidpassword, SetIsValidpassword] = useState(true)
-  const[IsValidConfirmPassword, SetIsValidConfirmPassword] = useState(true)
-  const[IsSelectedRole, SetIsSelectedRole] = useState(false)
+  const [userName, SetUserName] = useState("")
+  const [password, Setpassword] = useState("")
+  const [ConfirmPassword, SetConfirmPassword] = useState("")
+  const [RoleName, SetRoleName] = useState("")
+
+  const [IsValiduserName, SetIsValidUserName] = useState(true)
+  const [IsValidpassword, SetIsValidpassword] = useState(true)
+  const [IsValidConfirmPassword, SetIsValidConfirmPassword] = useState(true)
+  const [IsSelectedRole, SetIsSelectedRole] = useState(false)
 
 
-  const[ServerErrors, SetServerErrors] = useState("")
-  const[ISServerErrors, SetIsServerErrors] = useState(false)
+  const [ServerErrors, SetServerErrors] = useState("")
+  const [ISServerErrors, SetIsServerErrors] = useState(false)
   let nav = useNavigate();
-  function HandleRegister (params) {
-    if(!validator.isAlpha(userName) || validator.isEmpty(userName) && !validator.equals(password,ConfirmPassword))
-      {
-          SetIsValidUserName(false);
-          SetIsValidpassword(false);
-          SetIsValidConfirmPassword(false);
-          return;
-      }
-      if(!validator.isAlpha(userName) || validator.isEmpty(userName) )
-      {
-          SetIsValidUserName(false);
-          return;
-      }
-      if( validator.isEmpty(password))
-      {
-          SetIsValidpassword(false);
-          return;
-      }
-      if(!validator.equals(password,ConfirmPassword))
-      {
-          SetIsValidConfirmPassword(false);
-          return;
-      }
+  function HandleRegister(params) {
+    if (!validator.isAlpha(userName) || validator.isEmpty(userName) && !validator.equals(password, ConfirmPassword)) {
+      SetIsValidUserName(false);
+      SetIsValidpassword(false);
+      SetIsValidConfirmPassword(false);
+      return;
+    }
+    if (!validator.isAlpha(userName) || validator.isEmpty(userName)) {
+      SetIsValidUserName(false);
+      return;
+    }
+    if (validator.isEmpty(password)) {
+      SetIsValidpassword(false);
+      return;
+    }
+    if (!validator.equals(password, ConfirmPassword)) {
+      SetIsValidConfirmPassword(false);
+      return;
+    }
 
-      if(validator.equals(RoleName,'0')||validator.isEmpty(RoleName))
-      {
-       //   SetIsValidConfirmPassword(false);
-          return;
-      }
-   
-   register(userName,password,ConfirmPassword,RoleName).then(
-    ({data})=>{alert(data)},
-    ({response})=>{
-      console.log("e",response.data.description)
-      SetServerErrors(response.data.description);
-      SetIsServerErrors(true);
-    },
-   )
- 
+    if (validator.equals(RoleName, '0') || validator.isEmpty(RoleName)) {
+      //   SetIsValidConfirmPassword(false);
+      return;
+    }
+
+    register(userName, password, ConfirmPassword, RoleName).then(
+      ({ data }) => { alert(data) },
+      ({ response }) => {
+        console.log("e", response.data.description)
+        SetServerErrors(response.data.description);
+        SetIsServerErrors(true);
+      },
+    )
+
   }
   function HandelInputName(e) {
-      SetUserName(e.target.value);
-      SetIsValidUserName(true);
-      SetIsServerErrors(false);
+    SetUserName(e.target.value);
+    SetIsValidUserName(true);
+    SetIsServerErrors(false);
 
   }
   function HandelInputPassword(e) {
-      Setpassword(e.target.value);
-      SetIsValidpassword(true);
-      SetIsServerErrors(false);
+    Setpassword(e.target.value);
+    SetIsValidpassword(true);
+    SetIsServerErrors(false);
 
   }
 
@@ -80,25 +75,25 @@ export default function Register() {
     SetIsValidConfirmPassword(true);
     SetIsServerErrors(false);
 
- }
+  }
 
- function HadelSelectRole(e) {
-  SetRoleName(e.target.value);
-  SetIsSelectedRole(true)
-  
- }
+  function HadelSelectRole(e) {
+    SetRoleName(e.target.value);
+    SetIsSelectedRole(true)
+
+  }
   useEffect(() => {
     getAll().then(
-      ({ data }) => { setRoles(data) 
-        },
+      ({ data }) => {
+        setRoles(data)
+      },
       (err) => { alert(err) }
     )
-    
+
   }, [])
 
-  const navigateToLogIn=()=>
-  {
-      nav("/Login")
+  const navigateToLogIn = () => {
+    nav("/Login")
   }
 
 
@@ -116,93 +111,93 @@ export default function Register() {
                 <h5 class="card-title text-center mb-5 fw-light fs-5">
                   Register
                 </h5>
-              
-                  <div class="form-floating mb-3">
-                    <input
+
+                <div class="form-floating mb-3">
+                  <input
                     onChange={HandelInputName}
-                      type="text"
-                      class="form-control"
-                      id="floatingInputUsername"
-                      placeholder="myusername"
-                      required
-                      autofocus
-                    ></input>
-                    <label for="floatingInputUsername">Username</label>
-                    <div>
-                     {!IsValiduserName?<span className="text-danger"> User Name Is Requierd</span>:null}
-                    </div>
+                    type="text"
+                    class="form-control"
+                    id="floatingInputUsername"
+                    placeholder="myusername"
+                    required
+                    autofocus
+                  ></input>
+                  <label for="floatingInputUsername">Username</label>
+                  <div>
+                    {!IsValiduserName ? <span className="text-danger"> User Name Is Requierd</span> : null}
                   </div>
+                </div>
 
 
 
-                  <div class="form-floating mb-3">
-                    <input
+                <div class="form-floating mb-3">
+                  <input
                     onChange={HandelInputPassword}
-                      type="password"
-                      class="form-control"
-                      id="floatingPassword"
-                      placeholder="Password"
-                    ></input>
-                    <label for="floatingPassword">Password</label>
-                    <div>
-                     {!IsValidpassword?<span className="text-danger"> Password Is Requierd</span>:null}
-                    </div>
+                    type="password"
+                    class="form-control"
+                    id="floatingPassword"
+                    placeholder="Password"
+                  ></input>
+                  <label for="floatingPassword">Password</label>
+                  <div>
+                    {!IsValidpassword ? <span className="text-danger"> Password Is Requierd</span> : null}
                   </div>
+                </div>
 
-                  <div class="form-floating mb-3">
-                    <input
+                <div class="form-floating mb-3">
+                  <input
                     onChange={HandelInputConfirmPassword}
-                      type="password"
-                      class="form-control"
-                      id="floatingPasswordConfirm"
-                      placeholder="Confirm Password"
-                    ></input>
-                    <label for="floatingPasswordConfirm">
-                      Confirm Password
-                    </label>
-                    <div>
-                     {!IsValidConfirmPassword?<span className="text-danger"> Please Enter valid PassWord</span>:null}
-                    </div>
+                    type="password"
+                    class="form-control"
+                    id="floatingPasswordConfirm"
+                    placeholder="Confirm Password"
+                  ></input>
+                  <label for="floatingPasswordConfirm">
+                    Confirm Password
+                  </label>
+                  <div>
+                    {!IsValidConfirmPassword ? <span className="text-danger"> Please Enter valid PassWord</span> : null}
                   </div>
-                  <div class="form-floating mb-3">
-                    <select class="form-control" onChange={HadelSelectRole}>
-                      <option value="0" selected>Select Role</option>
-                      {
-                        Roles.map((v, i) => {
-                          return (
-                            <Fragment key={i}>
-                              <option value={v.name} >{v.name}</option>
-                            </Fragment>
-                          )
+                </div>
+                <div class="form-floating mb-3">
+                  <select className="form-select" onChange={HadelSelectRole}>
+                    <option value="0" selected>Select Role</option>
+                    {
+                      Roles.map((v, i) => {
+                        return (
+                          <Fragment key={i}>
+                            <option value={v.name} >{v.name}</option>
+                          </Fragment>
+                        )
 
-                        })
-                      }
+                      })
+                    }
 
 
 
 
-                    </select>
-                    <div>
-                     {!IsSelectedRole?<span className="text-danger">"please Select role</span>:null}
-                    </div>
+                  </select>
+                  <div>
+                    {!IsSelectedRole ? <span className="text-danger">"please Select role</span> : null}
                   </div>
-                  <div class="d-grid mb-2">
+                </div>
+                <div class="d-grid mb-2">
 
                   <div>
-                     {ISServerErrors?<span className="text-danger">{ServerErrors}</span>:null}
-                    </div>
-
-                    <button
-                    onClick={HandleRegister}
-                      class="btn btn-lg btn-primary btn-login fw-bold text-uppercase"
-                    >
-                      Register
-                    </button>
-                    <button className="btn btn-lg btn-primary btn-login fw-bold text-uppercase" onClick={navigateToLogIn}>
-  LogIn
-</button>
-
+                    {ISServerErrors ? <span className="text-danger">{ServerErrors}</span> : null}
                   </div>
+
+                  <button
+                    onClick={HandleRegister}
+                    class="btn btn-lg btn-primary btn-login fw-bold text-uppercase mb-3"
+                  >
+                    Register
+                  </button>
+                  <button className="btn btn-lg btn-primary btn-login fw-bold text-uppercase" onClick={navigateToLogIn}>
+                    LogIn
+                  </button>
+
+                </div>
 
 
               </div>
